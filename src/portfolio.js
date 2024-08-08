61a5feab9cfe58d8ed0cf1ee89b597644d431aa9
Purvang Vasani <purvangvasani4@gmail.com>
2024-08-08 22:33:18 +0530

    updated contact us page

--- src/portfolio.js
+++ src/portfolio.js
@@ -769,7 +769,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "pv.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am available on the social media. You can message me, I will reply within 24 hours. I can help you with MEAN, MERN projects and ElectronJs.",
   },
   blogSection: {
     title: "Blogs",
